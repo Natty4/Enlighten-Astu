f915264be5d7a3def5dc317a2c6a2b6cf005f9fc
Natnael <56716832+Natty4@users.noreply.github.com>
2022-03-26 12:50:37 +0300

    Update probarbot.py

--- probarbot.py
+++ probarbot.py
@@ -138,10 +138,10 @@ def start_over(update: Update, context: CallbackContext) -> int:
 
     if query:
         query.from_user.send_message(text = reply_text, reply_markup = markup_zero)
-        return FASTSERVE
+        return ConversationHandler.END
     else:
         update.message.reply_text(text=reply_text, reply_markup=markup_zero)
-        return FASTSERVE
+        return ConversationHandler.END
 
 
 """ --Download Sction-- """
